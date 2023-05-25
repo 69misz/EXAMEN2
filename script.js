9757b6ab93daf2729088d01585116ae47182153c
69misz <132626893+69misz@users.noreply.github.com>
2023-05-24 22:11:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// 5 
+let uno = true;
+let dos = "2";
+let tres = 3;
+let cuatro = null;
+let cinco;
+let seis = {numero: 6};
+let siete = [7];
+console.log(typeof uno);
+console.log(typeof dos);
+console.log(typeof tres);
+console.log(typeof cuatro);
+console.log(typeof cinco);
+console.log(typeof seis);
+console.log(typeof siete);
+
+
+// 6
+let datos = "nombreProducto " + "precio " + "direccionEnvio " + "país " + "costoEnvio ";
+let datos2 = "bell " + "50 " + "Santa Cruz " + "Bolivia " + "20"; 
+
+
+// 7
+let libreria = "React es una biblioteca Javascript de código abierto creada por Facebook."
+
+
+// 8
+console.log(libreria.length);
+
+
+// 9
+console.log(libreria.replace("React es una biblioteca", "ReactNative es un framework"));
+
+
+// 10
+let libreria2 = libreria.replace("JavaScript", "JavaScript");
+let libreria3 = libreria2.split(' ');
+console.log(libreria3[0].toUpperCase())
+console.log(libreria.substring(6,12));
+
+
+
+// 11
+let autos = "TOYOTA, SUZUKI, BMW, FORD, KIA, ISUZU, AUDI";
+console.log(autos.split(' '));
+
+
+// 12
+console.error(libreria.charCodeAt("R"));
+
+// 13 
+let frase = "HOLA MUNDO "
+console.log(frase.repeat(4));
+
+
+// 14
+console.log(\'Hola estudio /"Sistema"/y estoy llevando la materia de:'/
+
+
+// 15
+
+
+// 16
+console.log(libreria[8]);
+
+// 17
+let cadena = 'JavaScript';
---
+++ script_comentarios.js
@@ -0,0 +1,18 @@
+// 2
+//No te preocupes si no funciona bien.
+//Si todo estuviera correcto, serías despedido de tu trabajo.
+
+//Y no habria mucho que aprender.
+
+
+// 3
+//Primero resuelve el problema. Luego, escribe el código.
+
+
+// 4
+//Cualquier código tuyo que no hayas mirado en los últimos seis meses o más,
+//es como si lo hubiese escrito otra persona
+
+//-- Eagleson's Law 
+
+
